2ee022daea2021c2aa64ea164106dc4c07c24fe6
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-02-23 22:29:06 +1400

    exitContactApp implemented

--- src/ContactsAppSoftware/ContactAppFeatures.java
+++ src/ContactsAppSoftware/ContactAppFeatures.java
@@ -4,119 +4,121 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ContactAppFeatures implements Interface2 {
-   Scanner sc = new Scanner(System.in);
-   RegexFeature regexFeature = new RegexFeature();
+    Scanner sc = new Scanner(System.in);
+    RegexFeature regexFeature = new RegexFeature();
 
-   ArrayList<Person> myContactBook = new ArrayList<>();
+    ArrayList<Person> myContactBook = new ArrayList<>();
 
-   @Override
-   public void addNewContact() {
-      ArrayList<String> ContactNumbers = new ArrayList<>();
-      String firstName = "";
-      String lastName = "";
-      String emailAddress = "";
-      String phoneNumber = "";
-      System.out.println("Please enter the name of the Person");
-      System.out.println("Enter first Name: ");
-      firstName = sc.next();
+    @Override
+    public void addNewContact() {
+        ArrayList<String> ContactNumbers = new ArrayList<>();
+        String firstName = "";
+        String lastName = "";
+        String emailAddress = "";
+        String phoneNumber = "";
+        System.out.println("Please enter the name of the Person");
+        System.out.println("Enter first Name: ");
+        firstName = sc.next();
 
-      if (regexFeature.isFirstName(firstName)) {
-         System.out.println("Enter Last Name: ");
-         lastName = sc.next();
-         if (regexFeature.isLastName(lastName)) {
-            char reply;
-            do {
-               System.out.println("Enter your phone Number");
-               phoneNumber = sc.next();
-               if (regexFeature.isphoneNumber(phoneNumber)) {
-                  ContactNumbers.add(phoneNumber);
-               } else {
-                  System.out.println("..Syntax Error!..");
-                  break;
-               }
-               System.out.println("Would you like to add another contact number? (y/n): ");
-               reply = sc.next().toLowerCase().charAt(0);
+        if (regexFeature.isFirstName(firstName)) {
+            System.out.println("Enter Last Name: ");
+            lastName = sc.next();
+            if (regexFeature.isLastName(lastName)) {
+                char reply;
+                do {
+                    System.out.println("Enter your phone Number");
+                    phoneNumber = sc.next();
+                    if (regexFeature.isphoneNumber(phoneNumber)) {
+                        ContactNumbers.add(phoneNumber);
+                    } else {
+                        System.out.println("..Syntax Error!..");
+                        break;
+                    }
+                    System.out.println("Would you like to add another contact number? (y/n): ");
+                    reply = sc.next().toLowerCase().charAt(0);
+                }
+                while (reply == 'y');
+                System.out.println("Would you like to add email address? (y/n): ");
+                char reply1 = sc.next().toLowerCase().charAt(0);
+                if (reply1 == 'y') {
+                    System.out.println("Enter Your EmailAddress");
+                    emailAddress = sc.next();
+                    if (regexFeature.isEmailAddress(emailAddress)) {
+                        System.out.println("Email Address: " + emailAddress);
+
+                        if (regexFeature.isFirstName(firstName) && regexFeature.isLastName(lastName) && regexFeature.isEmailAddress(emailAddress) && regexFeature.isphoneNumber(phoneNumber)) {
+
+                            Person person = new Person(firstName, lastName, emailAddress, ContactNumbers);
+                            myContactBook.add(person);
+                        } else {
+                            System.out.println("\n..Please Enter Correct Name , PhoneNumber , email id - Syntax Error!..");
+                        }
+                    } else {
+                        System.out.println("..Syntax Error!..");
+                    }
+                }
+            } else {
+                System.out.println("..Syntax Error!..");
             }
-            while (reply == 'y');
-            System.out.println("Would you like to add email address? (y/n): ");
-            char reply1 = sc.next().toLowerCase().charAt(0);
-            if (reply1 == 'y') {
-               System.out.println("Enter Your EmailAddress");
-               emailAddress = sc.next();
-               if (regexFeature.isEmailAddress(emailAddress)) {
-                  System.out.println("Email Address: " + emailAddress);
-
-                  if (regexFeature.isFirstName(firstName) && regexFeature.isLastName(lastName) && regexFeature.isEmailAddress(emailAddress) && regexFeature.isphoneNumber(phoneNumber)) {
-
-                     Person person = new Person(firstName, lastName, emailAddress, ContactNumbers);
-                     myContactBook.add(person);
-                  } else {
-                     System.out.println("\n..Please Enter Correct Name , PhoneNumber , email id - Syntax Error!..");
-                  }
-               } else {
-                  System.out.println("..Syntax Error!..");
-               }
-            }
-         } else {
+        } else {
             System.out.println("..Syntax Error!..");
-         }
-      } else {
-         System.out.println("..Syntax Error!..");
-      }
-   }
-   @Override
-   public void viewAllContact() {
-      if (myContactBook.size() == 0) {
-         System.out.println("-------- * -------- * EMPTY - No Contacts yet *-------- * --------");
-      } else {
-         System.out.println("---Here are all your contacts---\n" +
-                 "-------- * -------- * -------- * --------");
-         for (int k = 0; k < myContactBook.size(); k++) {
-            if (k != 0) {
-               System.out.println("-------- * -------- **-------- * --------");
+        }
+    }
+
+    @Override
+    public void viewAllContact() {
+        if (myContactBook.size() == 0) {
+            System.out.println("-------- * -------- * EMPTY - No Contacts yet *-------- * --------");
+        } else {
+            System.out.println("---Here are all your contacts---\n" +
+                    "-------- * -------- * -------- * --------");
+            for (int k = 0; k < myContactBook.size(); k++) {
+                if (k != 0) {
+                    System.out.println("-------- * -------- **-------- * --------");
+                }
+                System.out.println("FirstName :" + myContactBook.get(k).getFirstName());
+                System.out.println("Last Name :" + myContactBook.get(k).getLastName());
+                System.out.println("Email Address :" + myContactBook.get(k).getEmailAddress());
+                System.out.println("Contacts Numbers: " + myContactBook.get(k).getContactNumbers());
+                System.out.println();
             }
-            System.out.println("FirstName :" + myContactBook.get(k).getFirstName());
-            System.out.println("Last Name :" + myContactBook.get(k).getLastName());
-            System.out.println("Email Address :" + myContactBook.get(k).getEmailAddress());
-            System.out.println("Contacts Numbers: " + myContactBook.get(k).getContactNumbers());
-            System.out.println();
-         }
-         System.out.println("-------- * -------- * -------- * --------");
-      }
-   }
+            System.out.println("-------- * -------- * -------- * --------");
+        }
+    }
 
     ArrayList<Integer> matched = new ArrayList<>();
 
 
-   @Override
-   public void searchForContact() {
+    @Override
+    public void searchForContact() {
 
-   }
+    }
 
-   @Override
-   public void deleteContact() {
-      int no = 1;
-      if (myContactBook.size() > 0) {
-         System.out.println("Here are all your Contacts : ");
-         for (int s = 0; s < myContactBook.size(); s++) {
-            System.out.println(no + ". " + myContactBook.get(s).getFirstName() + " " + myContactBook.get(s).getLastName());
-            no++;
-         }
-         int toDelete = sc.nextInt();
-         System.out.println("Press the number against the contact to delete it: " + toDelete);
-         System.out.println(myContactBook.get(toDelete - 1).getFirstName() + " " + myContactBook.get(toDelete - 1).getLastName() + "'s " + "contact deleted from list!");
-         myContactBook.remove(toDelete - 1);
-         myContactBook.trimToSize();
-         matched.trimToSize();
-      } else {
-         System.out.println("..........SORRY NO RESULTS FOUND............");
-      }
-   }
+    @Override
+    public void deleteContact() {
+        int no = 1;
+        if (myContactBook.size() > 0) {
+            System.out.println("Here are all your Contacts : ");
+            for (int s = 0; s < myContactBook.size(); s++) {
+                System.out.println(no + ". " + myContactBook.get(s).getFirstName() + " " + myContactBook.get(s).getLastName());
+                no++;
+            }
+            int toDelete = sc.nextInt();
+            System.out.println("Press the number against the contact to delete it: " + toDelete);
+            System.out.println(myContactBook.get(toDelete - 1).getFirstName() + " " + myContactBook.get(toDelete - 1).getLastName() + "'s " + "contact deleted from list!");
+            myContactBook.remove(toDelete - 1);
+            myContactBook.trimToSize();
+            matched.trimToSize();
+        } else {
+            System.out.println("..........SORRY NO RESULTS FOUND............");
+        }
+    }
 
 
    @Override
    public void exitContactApp() {
-
+           return;
+       }
    }
 
-}
+
