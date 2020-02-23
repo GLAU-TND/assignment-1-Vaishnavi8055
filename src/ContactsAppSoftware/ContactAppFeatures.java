651999ed61d21d83b3bca58253d1f766e2a1e563
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-02-23 21:55:01 +1400

    All Overriden methods created

--- src/ContactsAppSoftware/ContactAppFeatures.java
+++ src/ContactsAppSoftware/ContactAppFeatures.java
@@ -3,10 +3,30 @@ package ContactsAppSoftware;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class ContactAppFeatures {
+public class ContactAppFeatures implements Interface2 {
    Scanner sc=new Scanner(System.in);
    RegexFeature regexFeature=new RegexFeature();
 
    ArrayList<Person> myContactBook=new ArrayList<>();
+
+   @Override
+   public void addNewContact(){
+
+   }
+
+   @Override
+   public void viewAllContact() {
+
+   }
+
+   @Override
+   public void searchForContact() {
+
+   }
+
+   @Override
+   public void exitContactApp() {
+
+   }
 
 }
