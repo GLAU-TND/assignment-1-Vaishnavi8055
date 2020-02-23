01f8b1ba871cd73e6036e3ace82211b015033379
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-02-23 20:38:27 +1400

    method to get ContactNumbers of a person in Arraylist

--- src/ContactsAppSoftware/Interface1.java
+++ src/ContactsAppSoftware/Interface1.java
@@ -1,7 +1,10 @@
 package ContactsAppSoftware;
 
+import java.util.ArrayList;
+
 public interface Interface1 {
          String getFirstName();
          String getLastName();
          String getEmailAddress();
+         ArrayList<String> getContactNumbers();
 }
