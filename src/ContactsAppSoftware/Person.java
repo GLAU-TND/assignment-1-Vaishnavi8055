326611c26129eaa1571fa15310fe18b5886012d2
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-02-23 21:08:56 +1400

    Constructer (firstName,lastName,emailAddress,Arraylist of contactnumbers) is created

--- src/ContactsAppSoftware/Person.java
+++ src/ContactsAppSoftware/Person.java
@@ -2,16 +2,32 @@ package ContactsAppSoftware;
 
 import java.util.ArrayList;
 
-public class Person {
-  private String firstName;
-  private String lastName;
-  private String emailAddress;
-  private ArrayList<String> contactNumbers;
+public class Person implements Interface1{
+    private String FirstName;
+    private String LastName;
+    private String EmailAddress;
+    private ArrayList<String> ContactNumbers;
 
-    public Person(String firstName, String lastName, String emailAddress, ArrayList<String> contactNumbers) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.emailAddress = emailAddress;
-        this.contactNumbers = contactNumbers;
+    public Person(String firstName, String lastName, String emailAddress, ArrayList<String> ContactNumbers) {
+        FirstName = firstName;
+        LastName = lastName;
+        EmailAddress = emailAddress;
+        this.ContactNumbers = ContactNumbers;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public String getEmailAddress() {
+        return EmailAddress;
+    }
+
+    public ArrayList<String> getContactNumbers() {
+        return ContactNumbers;
     }
 }
