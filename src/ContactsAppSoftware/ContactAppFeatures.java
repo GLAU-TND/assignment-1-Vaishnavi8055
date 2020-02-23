f1f2eec8b0b6ef767fa1b77a7f32e1b0caf252e7
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-02-23 21:52:30 +1400

    ArrayList of person Class Created

--- src/ContactsAppSoftware/ContactAppFeatures.java
+++ src/ContactsAppSoftware/ContactAppFeatures.java
@@ -1,9 +1,12 @@
 package ContactsAppSoftware;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ContactAppFeatures {
    Scanner sc=new Scanner(System.in);
    RegexFeature regexFeature=new RegexFeature();
+
+   ArrayList<Person> myContactBook=new ArrayList<>();
 
 }
